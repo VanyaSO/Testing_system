1d2174960872f56a146a2a15cc10ae94da5b3a02
Ivan Ushachov <ushachovg324@gmail.com>
2024-05-14 12:42:02 +0300

    create main menu

--- Menu/Menu.h
+++ Menu/Menu.h
@@ -1,10 +1,19 @@
 #ifndef TESTING_SYSTEM_MENU_H
 #define TESTING_SYSTEM_MENU_H
+#include <iostream>
+using namespace std;
 
 
-class Menu {
-
+class Menu
+{
+protected:
+    static int getAction(int maxVarAction, int minVarAction=0);
+public:
+    static int getActionMenu(int, int=0);
+    static void mainMenu();
 };
+
+
 
 
 #endif
