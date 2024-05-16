1a72530f87b6600f766c8f6341445cd2b7dd92c1
TetherDen <bigdramashow730@gmail.com>
2024-05-16 23:57:22 +0300

    added Users

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+#include <string>
+#include "Users/User.h"
+#include "Users/Tester/Tester.h"
+#include "Users/Admin/Admin.h"
+
+
+
 int main()
 {
+
+    Tester tst("MyLogin", "MyPass", "Role", "MyName", "MySname", "MyLastName", "MyAddress", "0961112233");
+    tst.show();
+
+    Admin adm("LOGIN", "PASSWORD", "ROLE");
+    adm.show();
+
 
     return 0;
 }
