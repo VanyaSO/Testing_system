f276978fc7e9a1a08450d26b9ae393e7f4134296
Ivan Ushachov <ushachov.ivan@armar.solutions>
2024-05-21 11:55:24 +0300

    add file config.cpp and updated config.h

---
+++ common/config.cpp
@@ -0,0 +1,9 @@
+#include "./config.h"
+
+const char* ERROR_COLOR = "\033[1;31m";
+const char* SUCCESSFUL_COLOR = "\033[1;32m";
+const char* WARNING_COLOR = "\033[1;33m";
+const char* RESET_COLOR = "\033[0m";
+
+User* user = nullptr;
+vector<User*> arrayUser;
